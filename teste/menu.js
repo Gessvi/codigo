fa20682c743f130f44221042e5f7902470a8ef6e
Gessica Vasconcelos <143418965+Gessvi@users.noreply.github.com>
2024-10-21 22:51:24 -0300

    Delete teste directory

--- teste/menu.js
+++
@@ -1,9 +0,0 @@
-// Seleciona o botão de expandir e o menu lateral
-const btnExpandir = document.querySelector('.btn-expandir');
-const menuLateral = document.querySelector('.menu-lateral');
-
-// Adiciona um evento de clique ao botão
-btnExpandir.addEventListener('click', function() {
-    // Alterna a classe 'expandido' no menu lateral
-    menuLateral.classList.toggle('expandido');
-});
